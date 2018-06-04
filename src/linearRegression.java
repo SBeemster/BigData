65be943a1a55e656b062edc1009ce399d1521f2f
jorisvdinther <j.vandinther@kw1c.nl>
2018-06-04 21:14:06 +0200

    if statement verwijderd en ipv JSON object gebruik maken van het bestaande BSON object

--- src/linearRegression.java
+++ src/linearRegression.java
@@ -1,8 +1,6 @@
 import java.util.List;
 import org.apache.commons.math3.stat.regression.SimpleRegression;
 import org.json.JSONException;
-import org.json.JSONObject;
-
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
@@ -25,29 +23,21 @@ public class linearRegression {
 		List<DBObject> items =toelatingenPerJaar.find(query).toArray();
 		double[][] regressionArray = new double[items.size()][2];		
 				
-			try {
-				for(int i=0; i < items.size(); i++) {
-					JSONObject JSON = new JSONObject(items.get(i).toString());
-					
-					if(JSON.get("_id") != "NaN") {
-						regressionArray[i][0] =(int)JSON.getDouble("_id");
-						regressionArray[i][1] =(int)JSON.getDouble("value");
-					}
-				}
-		
-				SimpleRegression regression = new SimpleRegression();
-				
-				regression.addData(regressionArray);
-				System.out.println("Intercept: "+ regression.getIntercept());
-				System.out.println("Slope: " + regression.getSlope());
-				System.out.println("Slope Standard Error: " +regression.getSlopeStdErr());
-				
-				
-				System.out.println("Voorspelling voor het jaar 2040 is het aantal voertuigen: " + regression.predict(2040));
-				
-			} catch (JSONException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}		
+			for(int i=0; i < items.size(); i++) {
+				double jaar = (double) items.get(i).get("_id");
+				regressionArray[i][0] = (int) jaar;
+				double value = (double) items.get(i).get("value");
+				regressionArray[i][1] =(int) value;
+			}
+
+			SimpleRegression regression = new SimpleRegression();
+			
+			regression.addData(regressionArray);
+			System.out.println("Intercept: "+ regression.getIntercept());
+			System.out.println("Slope: " + regression.getSlope());
+			System.out.println("Slope Standard Error: " +regression.getSlopeStdErr());
+			
+			
+			System.out.println("Voorspelling voor het jaar 2040 is het aantal voertuigen: " + regression.predict(2040));		
 	}
 }
